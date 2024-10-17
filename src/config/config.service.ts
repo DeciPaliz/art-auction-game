740d47412d6010d1219132a01365393a61dfd3c7
DeciPaliz <unat7919@gmail.com>
2024-10-17 18:33:16 +0300

    separate .env files for development/production and https

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService as NestConfigService } from '@nestjs/config';
 
 interface ConfigEnvironmentVariables {
+  MODE: string;
   POSTGRES_USER: string;
   POSTGRES_PASSWORD: string;
   POSTGRES_DB: string;
@@ -10,10 +11,19 @@ interface ConfigEnvironmentVariables {
 
 @Injectable()
 export class ConfigService extends NestConfigService<ConfigEnvironmentVariables> {
+  constructor(internalConfig?: Record<string, any>) {
+    super(internalConfig);
+
+    console.log('running in ' + this.getOrThrow('MODE') + ' mode');
+  }
   static game = {
     maxPlayersMax: 5,
     maxPlayersMin: 3,
     passwordLengthMax: 30,
     passwordLengthMin: 0,
   };
+
+  static isDevelopment() {
+    return process.env.MODE === 'development';
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { NestApplicationOptions, ValidationPipe } from '@nestjs/common';
+import { readFileSync } from 'node:fs';
+import { ConfigService } from './config/config.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  let options: NestApplicationOptions;
+  if (!ConfigService.isDevelopment()) {
+    options.httpsOptions = {
+      key: readFileSync('./secret/key.pem'),
+      cert: readFileSync('./secret/cert.pem'),
+    };
+  }
+  const app = await NestFactory.create(AppModule, options);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(3000);
 }
