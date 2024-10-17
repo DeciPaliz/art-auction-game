eed83c1228465caaa849f7bc50efeaae8194becb
DeciPaliz <unat7919@gmail.com>
2024-10-17 22:36:27 +0300

    restructure frontend

--- frontend/src/components/app/App.tsx
+++ frontend/src/components/app/App.tsx
@@ -1,14 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
-import { Navbar } from './Navbar';
+import { Navbar } from '../navbar/Navbar';
 import { lazy, Suspense } from 'react';
 import { Spinner } from 'reactstrap';
 
-const Root = lazy(() => import('./routes/Root'));
-const Join = lazy(() => import('./routes/Join'));
-const Play = lazy(() => import('./routes/Play'));
-const Upload = lazy(() => import('./routes/Upload'));
-const SignIn = lazy(() => import('./routes/SignIn'));
-const NotFound = lazy(() => import('./routes/NotFound'));
+const Root = lazy(() => import('../../routes/root/Root'));
+const Join = lazy(() => import('../../routes/join/Join'));
+const Play = lazy(() => import('../../routes/play/Play'));
+const Upload = lazy(() => import('../../routes/upload/Upload'));
+const SignIn = lazy(() => import('../../routes/signin/SignIn'));
+const NotFound = lazy(() => import('../../routes/notfound/NotFound'));
 
 export const App = () => {
   const fallback = (
